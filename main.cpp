9f18241ba83caf57a1040a935c1e624c0f9f6e54
Karol Briegmann <kbriegmann@wp.pl>
2023-02-26 20:35:58 +0100

    Some modyfication

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 int main(){
 
-	std::cout<<"Hello, I'm RomanNumbers project"<<std::endl;
+	std::string str{};
 
+	std::cout<<"Hello, I'm RomanNumbers project"<<std::endl;
+	std::cout<<"Put some text here!"<<std::endl;
+
+	std::cin>>str;
+
+	std::cout<<"Size of variable str: "<<str.size()<<std::endl;
 	return 0;
 }
