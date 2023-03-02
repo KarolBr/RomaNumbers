173167fc76716b3d26f0826fe1c20cfdda5ccd88
Karol Briegmann <kbriegmann@wp.pl>
2023-03-02 20:29:38 +0100

    Improve getRomanNumber method

--- src/RomanNumbers.cpp
+++ src/RomanNumbers.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 std::string getRomanNumber(std::string decimalNumber)
 {
     std::string romanNumber{};
-    auto sizeOfDecimalNumber = decimalNumber.size();
+    int sizeOfDecimalNumber = decimalNumber.size();
 
     //podzielić decimalNumber na składowe: units,tens, hundreds and thousands
 
@@ -14,6 +15,45 @@ std::string getRomanNumber(std::string decimalNumber)
     std::vector<std::string> hundreds = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
     std::string thousand = "M";
 
+    std::string my_units{};
+    std::string my_tens{};
+    std::string my_handreds{};
+    std::string my_thousends{};
+    
+    char singleNumbers[5];
+
+
+    //int sizeOfString = number.size();
+
+    for(int i=0;i<sizeOfDecimalNumber;i++){
+        singleNumbers[i] = decimalNumber.at(i);
+    }
+
+
+    //thousends
+    int thousends_ = singleNumbers[0] - '0';
+    
+    
+    for(int i=0;i<thousends_;i++)
+        my_thousends += "M";
+
+
+    //hundreds
+    int hundreds_ = singleNumbers[1] - '0';
+    my_handreds = hundreds[hundreds_-1];
+
+
+    //tens
+    int tens_ = singleNumbers[2] - '0';
+    my_tens = tens[tens_-1];
+
+    //units
+    int units_ = singleNumbers[3] - '0';
+    my_units = units[units_-1];
+
+    romanNumber = my_thousends+my_handreds+my_tens+my_units;
+
+/*
     switch (sizeOfDecimalNumber)
     {
     case 1:
@@ -32,4 +72,7 @@ std::string getRomanNumber(std::string decimalNumber)
         return "outOfRange";
         break;
     }
+    */
+
+   return romanNumber;
 }
